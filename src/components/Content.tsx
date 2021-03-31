c857981cfab5a06d7b570e5f02ecb6d1a49ee0b4
odairdev <odair.dev@gmail.com>
2021-03-31 16:52:18 -0300

    Challenge Finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+import '../styles/content.scss';
+
+interface ContentProps {
+  SelectedGenreTitle: string;
+
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+}
+
+export function Content({SelectedGenreTitle, movies}: ContentProps) {
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {SelectedGenreTitle}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+  
 }
